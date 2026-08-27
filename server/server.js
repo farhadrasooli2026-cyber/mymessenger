e2f95c5d3a2da6e361c04d65117abc41057b1c08
Farhad <Farhadrasooli2026@gmail.com>
2026-08-27 09:34:07 +0300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+app.use(express.static(__dirname));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("MyMessenger Server is running!");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("message", (message) => {
+    console.log("Message:", message);
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
