276a824ad5f79d60b1cf76b9698d90e89069ff3e
Farhad <Farhadrasooli2026@gmail.com>
2026-08-29 17:25:14 +0300

    MyMessenger project

--- server/update-db.js
+++
@@ -1,4 +0,0 @@
-const db = require("./database");
-try { db.prepare( "ALTER TABLE users ADD COLUMN gender TEXT DEFAULT 'female'" ).run();
-console.log("Gender column added successfully");
-} catch (error) { if (error.message.includes("duplicate column name")) { console.log("Gender column already exists"); } else { console.error(error); } }
